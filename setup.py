b4b2c525a26dec5fbd9a3e78bb33b4cfe8bbd339
jesperstroem <js@ece.au.dk>
2023-09-05 16:42:19 +0200

    Bumped patch

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='mlarchitectures',
-    version='0.1.1',    
+    version='0.1.2',    
     description='Shared ML Architectures',
     url='https://gitlab.au.dk/tech_ear-eeg/ml_architectures',
     author='Jesper Strøm',
