4167ff24165bab2b8a7c2aef2fb09bf6c2fe0efb
jesperstroem <js@ece.au.dk>
2023-09-05 15:45:58 +0200

    Added package setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='ml_architectures',
+    version='0.1.0',    
+    description='Shared ML Architectures',
+    url='https://gitlab.au.dk/tech_ear-eeg/ml_architectures',
+    author='Jesper Strøm',
+    author_email='js@ece.au.dk',
+    packages=['lseqsleepnet'],
+    install_requires=['torch',
+                      'numpy']
+)
