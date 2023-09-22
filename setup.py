df0f62fed7da25ae6d3c47537d9a1f6598a5f1dd
Jesper Strøm <js@ece.au.dk>
2023-09-22 13:40:47 +0200

    Develop

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup
 
 setup(
     name="mlarchitectures",
-    version="0.2.2",
+    version="0.2.3",
     description="Shared ML Architectures",
     url="https://gitlab.au.dk/tech_ear-eeg/ml_architectures",
     author="Jesper Strøm",
     author_email="js@ece.au.dk",
     packages=[
         "ml_architectures",
+        "ml_architectures.common",
         "ml_architectures.lseqsleepnet",
         "ml_architectures.usleep",
         "ml_architectures.common",
